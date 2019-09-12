b1a5372a38fdb9e073a2cabf1b20712e6e296463
bangbang93 <bangbang93@163.com>
2019-09-12 16:18:38 +0800

    优化service

--- src/app/core/services/database/database.service.ts
+++ src/app/core/services/database/database.service.ts
@@ -1,16 +1,16 @@
 import {Injectable} from '@angular/core';
 import {Loki} from '@lokidb/loki';
-import {remote} from 'electron';
 import {join} from 'path';
 import {FSStorage} from '@lokidb/fs-storage';
+import {ElectronService} from '..';
 
 @Injectable()
 export class DatabaseService {
   public readonly db: Loki;
   private readonly dir: string;
 
-  public constructor() {
-    this.dir = remote.app.getPath('userData');
+  public constructor(electronService: ElectronService) {
+    this.dir = electronService.remote.app.getPath('userData');
     this.db = new Loki(join(this.dir, 'bmcl.db'));
   }
 
