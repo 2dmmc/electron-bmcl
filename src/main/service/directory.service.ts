5933c561cabaff347988b8492202d59e96ee3b3a
bangbang93 <bangbang93@163.com>
2019-09-12 23:02:22 +0800

    DirectoryService

--- src/main/service/directory.service.ts
+++ src/main/service/directory.service.ts
@@ -1,7 +1,19 @@
 import {DirectoryModel} from '../model/directory.model';
 
 export class DirectoryService {
-  public static async list() {
+  public static async list(): Promise<DirectoryModel[]> {
     return DirectoryModel.findAll();
+  }
+
+  public static async add(name: string, path: string): Promise<DirectoryModel> {
+    return DirectoryModel.create({
+      name, path
+    });
+  }
+
+  public static async remove(name: string): Promise<void> {
+    return DirectoryModel.destroy({
+      where: {name},
+    });
   }
 }
