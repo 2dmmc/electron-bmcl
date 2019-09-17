851945c7cc5342660f036b9ab6a3356a86a05e1a
sdjnmxd <i@mxd.moe>
2019-09-17 18:58:12 +0800

    games template

--- src/app/games/games.module.ts
+++ src/app/games/games.module.ts
@@ -4,12 +4,14 @@ import {CommonModule} from '@angular/common';
 import {GamesRoutingModule} from './games-routing.module';
 
 import {GamesComponent} from './games.component';
+import {MaterialModule} from '../app.module';
 
 @NgModule({
   declarations: [
     GamesComponent
   ],
   imports: [
+    MaterialModule,
     CommonModule,
     GamesRoutingModule
   ],
