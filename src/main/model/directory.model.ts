c88fec5577fc6f03e19c5aacecf44ef28d0c7987
bangbang93 <bangbang93@163.com>
2019-09-18 18:55:25 +0800

    fix startup

--- src/main/model/directory.model.ts
+++ src/main/model/directory.model.ts
@@ -4,8 +4,8 @@ import {Column, Default, Model, NotNull, PrimaryKey, Table, Unique} from 'sequel
   modelName: 'directory',
 })
 export class DirectoryModel extends Model<DirectoryModel> {
-  @PrimaryKey @Column @NotNull name: string;
-  @Unique @Column @NotNull path: string;
-  @Column @Default(false) @NotNull isFavorite: boolean;
-  @Column @Default('') @NotNull currentVersion: string;
+  @PrimaryKey @NotNull @Column({allowNull: false}) name: string;
+  @Unique @NotNull @Column({allowNull: false}) path: string;
+  @Default(false) @NotNull @Column({allowNull: false}) isFavorite: boolean;
+  @Default('') @NotNull @Column({allowNull: false}) currentVersion: string;
 }
