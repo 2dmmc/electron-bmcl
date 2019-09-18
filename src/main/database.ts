c9fd3424da41a15497c4f4c562a2652201697c68
bangbang93 <bangbang93@163.com>
2019-09-18 19:01:07 +0800

    移除测试代码

--- src/main/database.ts
+++ src/main/database.ts
@@ -3,8 +3,6 @@ import {Sequelize} from 'sequelize-typescript';
 import {DirectoryModel} from './model/directory.model';
 import {SettingModel} from './model/setting.model';
 import {app} from 'electron';
-import {DirectoryService} from './service/directory.service';
-import {GameService} from './service/game.service';
 
 export const sequelize = new Sequelize({
   dialect: 'sqlite',
@@ -15,14 +13,3 @@ sequelize.addModels([
   DirectoryModel,
   SettingModel,
 ]);
-
-DirectoryService.getByName('test')
-  .then(async (doc) => {
-    if (!doc) {
-      doc = await DirectoryService.add('test', 'E:\\BMCL');
-    }
-    const gameService = new GameService(doc);
-    const list = await gameService.listVersions();
-    console.log(list);
-  })
-  .catch(console.error);
