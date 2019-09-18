06554af4781445216f465c98e9bcfbb3ee27e57a
sdjnmxd <i@mxd.moe>
2019-09-18 18:25:39 +0800

    完善IGame模型

--- src/app/@model/game.interface.ts
+++ src/app/@model/game.interface.ts
@@ -2,6 +2,8 @@ import {IVersion} from './version.interface';
 
 export interface IGame {
   name: string;
+  path: string;
   currentVersion: string;
   versions: IVersion[];
+  isFavorite?: boolean;
 }
