45e72884a2ab928704dde1237448602a38bb2afd
bangbang93 <bangbang93@163.com>
2019-09-19 16:36:18 +0800

    version json type

--- src/main/database.ts
+++ src/main/database.ts
@@ -4,9 +4,11 @@ import {DirectoryModel} from './model/directory.model';
 import {SettingModel} from './model/setting.model';
 import {app} from 'electron';
 
+const dbPath = join(app.getPath('userData'), 'bmcl.db');
+
 export const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: join(app.getPath('userData'), 'bmcl.db'),
+  storage: dbPath,
 });
 
 sequelize.addModels([
--- src/main/service/game.service.ts
+++ src/main/service/game.service.ts
@@ -18,3 +18,98 @@ export class GameService {
       .map((name) => ({name}));
   }
 }
+
+interface IVersionRule {
+  action: string;
+  features?: {[key: string]: boolean};
+  os: {
+    name: string;
+    version: string;
+  };
+}
+
+interface IVersionArgumentRule {
+  rules: IVersionRule[];
+  value: string;
+}
+
+interface IVersionArguments {
+  game: Array<string | IVersionArgumentRule>;
+  jvm: Array<string | IVersionArgumentRule>;
+}
+
+interface IVersionAsset {
+  id: string;
+  sha1: string;
+  size: number;
+  totalSize: number;
+  url: string;
+}
+
+interface IVersionDownload {
+  sha1: string;
+  size: number;
+  url: string;
+}
+
+interface IVersionDownloads {
+  client: IVersionDownload;
+  server: IVersionDownload;
+}
+
+interface IVersionArtifact {
+  path: string;
+  sha1: string;
+  size: number;
+  url: string;
+}
+
+interface IVersionLibrary {
+  name: string;
+  downloads: {
+    artifact: IVersionArtifact;
+    classifiers: {
+      javadoc: IVersionArtifact;
+      'natives-linux': IVersionArtifact;
+      'natives-macos': IVersionArtifact;
+      'natives-windows': IVersionArtifact;
+      sources: IVersionArtifact;
+    }
+    natives: {
+      osx: string;
+      linux: string;
+      windows: string;
+    }
+    extract: {
+      exclude: string[];
+    }
+  };
+  rules: IVersionRule[];
+}
+
+interface IVersionLogging {
+  client: {
+    argument: string
+    file: {
+      id: string;
+      sha1: string;
+      size: string;
+      url: string;
+    };
+    type: string;
+  };
+}
+
+interface IVersionSchema {
+  arguments: IVersionArguments;
+  assetIndex: IVersionAsset;
+  assets: string;
+  downloads: IVersionDownloads;
+  libraries: IVersionLibrary[];
+  logging: IVersionLogging;
+  mainClass: string;
+  minimumLauncherVersion: number;
+  releaseTime: string;
+  time: string;
+  type: string;
+}
